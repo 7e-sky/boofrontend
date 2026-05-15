a95c53cf3afec7e58604474c99effc007d19ac4f
7e-sky <webmaster@7e-sky.com>
2026-05-15 23:35:54 +0100

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -1,8 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default axios.create({
-  //baseURL: `http://192.168.1.124:8000/`
-  //baseURL: `http://192.168.11.108:8000/`,
-  baseURL: `https://bps.boopursal.com/`,
-    //baseURL: `https://api.boopursal.com/`,
+// Utilisation de l'adresse de production STABLE du Backend ou locale
+const URL_BACKEND = window.location.hostname === 'localhost' ? "http://localhost:3002" : "https://bps.boopursal.com/";
+
+console.log('🚀 SYSTEME : Forçage de l\'Agent API sur : ' + URL_BACKEND);
+
+const instance = axios.create({
+    baseURL: URL_BACKEND,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
+
+export default instance;
